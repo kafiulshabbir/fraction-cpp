1a206fad71c808f247e2ce57ac9a5353113d9c81
Kafi Ul Shabbir <58966086+kafiulshabbir@users.noreply.github.com>
2020-11-20 03:18:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,73 +1,73 @@
 #include <iostream>
-#include "fraction.h"
+#include "frac.h"
 using namespace std;
 int main()
 {
     //Testing default constructor
-    fraction a;
-    cout << "Fraction a is: " << a << '\n';
+    frac a;
+    cout << "frac a is: " << a << '\n';
 
     //Testing parameterized (double) constructor
-    fraction b(2, -4);
-    cout << "Fraction b is: " << b << '\n';
-    fraction c(-3, 7);
-    cout << "Fraction c is: " << c << '\n';
+    frac b(2, -4);
+    cout << "frac b is: " << b << '\n';
+    frac c(-3, 7);
+    cout << "frac c is: " << c << '\n';
 
     //Testing parameterized (single) constructor
-    fraction d(-8);
-    cout << "Fraction d is: " << d << '\n';
+    frac d(-8);
+    cout << "frac d is: " << d << '\n';
 
     //Testing copy constructor
-    fraction e(b);
-    cout << "Fraction e is: " << e << '\n';
+    frac e(b);
+    cout << "frac e is: " << e << '\n';
 
     //Testing = operator
     b = c;
-    cout << "Fraction b is: " << b << '\n';
+    cout << "frac b is: " << b << '\n';
 
     //Testing unary plus operator;
-    cout << "Fraction +b is: " << +b << '\n';
-    cout << "Fraction b is: " << b  << '\n';
+    cout << "frac +b is: " << +b << '\n';
+    cout << "frac b is: " << b  << '\n';
 
     //Testing unary minus operator
-    cout << "Fraction -b is: " << -b << '\n';
-    cout << "Fraction b is: " << b << '\n';
+    cout << "frac -b is: " << -b << '\n';
+    cout << "frac b is: " << b << '\n';
 
     //Testing += operator
     c += d;
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac c is: " << c << '\n';
 
      //Testing -= operator
     c -= d;
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac c is: " << c << '\n';
 
      //Testing *= operator
     c *= d;
-    cout << "Fraction c*=d is: " << c << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing /= operator
     c /= d;
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing ++x operator
     b = ++c;
-    cout << "Fraction b = ++c is: " << b << '\n';
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac b is: " << b << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing --x operator
     b = --c;
-    cout << "Fraction b== --c is: " << b << '\n';
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac b is: " << b << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing x++ operator
     b = c++;
-    cout << "Fraction b = c++ is: " << b << '\n';
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac b is: " << b << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing x-- operator
     b = c--;
-    cout << "Fraction b = c-- is: " << b << '\n';
-    cout << "Fraction c is: " << c << '\n';
+    cout << "frac b is: " << b << '\n';
+    cout << "frac c is: " << c << '\n';
 
     //Testing == operator
     cout << "Is a == b: " << (a == b) << '\n';
@@ -89,15 +89,15 @@ int main()
 
     //Testing + operator
     a = b + e;
-    cout << "Fraction a is: " << a << '\n';
+    cout << "frac a is: " << a << '\n';
 
      //Testing - operator
     a = b - e;
-    cout << "Fraction a is: " << a << '\n';
+    cout << "frac a is: " << a << '\n';
 
     //Testing * operator
     a = b * e;
-    cout << "Fraction a is: " << a << '\n';
+    cout << "frac a is: " << a << '\n';
 
     //Testing / operator
 
